b31563b1fb255a2a1fea9069e2d468f354617dd2
aingc <caing@pdx.edu>
2017-01-28 22:14:24 -0800

    Got rid of redundancy

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var server = express();
 
-server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname));
 
 server.listen(process.env.PORT || 8080);
 console.log("Local Host Started");
